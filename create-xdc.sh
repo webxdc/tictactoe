68820bf6311ed8f99e3863a97d80266e4ef4594c
Simon Laux <mobile.info@simonlaux.de>
2022-06-02 14:22:09 +0200

    save a few bytes on the icon and hardcode name in script

--- create-xdc.sh
+++ create-xdc.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 
-case "$1" in
-    "-h" | "--help")
-        echo "usage: ${0##*/} [PACKAGE_NAME]"
-        exit
-        ;;
-    "")
-        PACKAGE_NAME=${PWD##*/} # '##*/' removes everything before the last slash and the last slash
-        ;;
-    *)
-        PACKAGE_NAME=${1%.xdc} # '%.xdc' removes the extension and allows PACKAGE_NAME to be given with or without extension
-        ;;
-esac
+PACKAGE_NAME=tictactoe
 
 rm "$PACKAGE_NAME.xdc" 2> /dev/null
 zip -9 --recurse-paths "$PACKAGE_NAME.xdc" * --exclude README.md webxdc.js webxdc.d.ts "*.sh" "*.xdc" screenshot.png LICENSE
